409ea32a3f9499f6458faf218feb71b1c5d47595
Unknown <sanchez.dav90@gmail.com>
2018-03-07 17:54:57 -0600

    Tests finished

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,25 @@
 class Task
-  attr_accessor :size, :completed
+  attr_accessor :size, :completed_at
 
   def initialize(options = {})
-    @completed = options[:completed]
+    mark_completed(options[:completed]) if options[:completed]
     @size = options[:size]
   end
 
-  def mark_completed
-    @completed = true
+  def mark_completed(date = nil)
+    @completed_at = (date || Time.current)
   end
 
   def complete?
-    @completed
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > Project.velocity_length_in_days.days.ago
+  end
+
+  def points_toward_velocity
+    if part_of_velocity? then size else 0 end
   end
 end
